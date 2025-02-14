3a87bb2a2a4dab487ec802b3c8aadabb7bb29694
jean05-beep <jean05_pierre96@hotmail.com>
2025-02-14 12:39:16 -0500

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,10 @@
+function mostrarContenido() {
+    // Obtener el div con el contenido de la carta
+    var carta = document.getElementById("carta");
+    
+    // Mostrar el contenido con animación
+    carta.style.display = "block";
+    carta.style.transform = "scale(1)";
+    carta.style.opacity = "1";
+
+}
